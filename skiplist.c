10f96244352ddc6deb6a96148e48b65edeb35d79
KrahnacK <sylvain.geneves@gmail.com>
2013-05-20 16:12:02 +0200

    licensing stuff...

--- skiplist.c
+++ skiplist.c
@@ -1,3 +1,13 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Sylvain Genevès wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ * ----------------------------------------------------------------------------
+ */
+
+
 #define _GNU_SOURCE
 #include <stdlib.h>
 #include <assert.h>
